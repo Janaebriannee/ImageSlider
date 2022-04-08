8a51d773de49bbb86272f3e861d402900b13d811
JanaePerez <>
2022-04-08 13:43:34 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const prev = document.querySelector('.prev'); 
+const next = document.querySelector('.next');
+const images = document.querySelector('.carousel').children;
+const totalImages = images.length;
+let index = 0;
+
+prev.addEventListener('click', () => {
+  nextImage('prev');
+})
+next.addEventListener('click', () => {
+  nextImage('next');
+})
+function nextImage(direction) {
+  if(direction == 'next') {
+    index++; //index=index+1
+    if(index == totalImages) {
+      index = 0;
+    }
+  } else {
+    if(index == 0) {
+      index = totalImages - 1;
+    } else {
+      index--; //index=index-1
+    }
+  }
+
+  // Putting the image at the index position in spotlight/visible.
+  for(let i = 0; i < images.length; i++) {
+    images[i].classList.remove('main');
+  }
+  
+  images[index].classList.add('main');
+}
